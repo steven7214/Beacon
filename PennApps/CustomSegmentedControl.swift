ab19148e454de50417c256631b179e6043488d99
hdong3030 <hldong@andrew.cmu.edu>
2019-09-07 02:08:04 -0400

    hi

---
+++ PennApps/CustomSegmentedControl.swift
@@ -0,0 +1,22 @@
+//
+//  CustomSegmentedControl.swift
+//  PennApps
+//
+//  Created by Helen Dong on 9/7/19.
+//  Copyright © 2019 Helen Dong. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CustomSegmentedControl: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
