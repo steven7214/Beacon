2decbad27b37d3963662840a9cbc87577ccbc19f
hdong3030 <hldong@andrew.cmu.edu>
2019-09-06 23:03:07 -0400

    location for user

--- PennApps/PennApps/FirstViewController.swift
+++ PennApps/PennApps/FirstViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, CLLocationManagerDelegate {
+    
+    //Map
+    @IBOutlet weak var map: MKMapView!
+    
+    let manager = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+       let location = locations[0]
+        
+        let span:MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.1,longitudeDelta: 0.1)
+        
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        
+        let region:MKCoordinateRegion = MKCoordinateRegion(center: myLocation, span: span)
+        
+        map.setRegion(region, animated: false)
+        
+        self.map.showsUserLocation = true
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
     }
 
 
